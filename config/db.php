9b9fe66b13fdd2573d442efa95e738ba648ef68c
Lynxiel <linansk@inbox.ru>
2015-01-29 13:46:18 +0300

    database config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-
+//http://linansk.heliohost.org/cpanel
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=85.10.205.173:3306;dbname=ivatch',
+    'username' => 'ivatchuser',
+    'password' => '4332a6',
     'charset' => 'utf8',
 ];
