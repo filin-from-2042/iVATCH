c47f3de7e409b4ab1b67a51f6a3661f992ac9d8f
filin-from-2042 <filin2042@gmail.com>
2015-02-02 23:52:46 +0300

    carousel and parallax processing

---
+++ views/layouts/jumbotron.php
@@ -0,0 +1,17 @@
+<?php
+    use yii\helpers\Url;
+?>
+<div class="jumbotron">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="brand">
+                    <div class="wrap">
+                        <h1>
+                            <a href="<?=Url::home();?>">iva <span class="glyphicon glyphicon-facetime-video"></span>tch</a>
+                        </h1>
+                        <div class="slogan">Streaming service</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
